e21030604287f4cb72cb0c29659aab06494c0d6a
A-leonka <samedy666@gmail.com>
2021-03-16 18:32:52 +0300

    feat: add new code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+if(!Array.isArray(members)) {
+return false
+}
+let array = members.filter(function(el) {
+  if(typeof el === 'string') {
+  return true
+  }
+  })
+
+let result = ''
+array.map(function(a) {
+  return result += (a.trim())[0].toUpperCase()
+})
+return result.split('').sort().join('')
 };
