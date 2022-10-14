c7680f948648b4587914e9ef6fc69b39867cc620
A-leonka <samedy666@gmail.com>
2022-10-15 00:18:01 +0300

    feat: add new code

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let count = 0
+  let matrixArr = []
+  for (let i = 0; i < matrix.length; i++) {
+    let arr = []
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr.push('pig')
+    }
+    matrixArr.push(arr)
+    arr = []
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i > 0 && j > 0) {
+        if (matrix[i - 1][j - 1] === true) {
+          count++
+        }
+      }
+      if (i > 0) {
+        if (matrix[i - 1][j] === true) {
+          count++
+        }
+      }
+
+      if (i > 0 && j < matrix[i].length - 1) {
+        if (matrix[i - 1][j + 1] === true) {
+          count++
+        }
+      }
+
+      if (j > 0) {
+        if (matrix[i][j - 1] === true) {
+          count++
+        }
+      }
+
+      if (j < matrix[i].length - 1) {
+        if (matrix[i][j + 1] === true) {
+          count++
+        }
+      }
+
+      if (i < matrix.length - 1 && j > 0) {
+        if (matrix[i + 1][j - 1] === true) {
+          count++
+        }
+      }
+
+      if (i < matrix.length - 1) {
+        if (matrix[i + 1][j] === true) {
+          count++
+        }
+      }
+
+      if (i < matrix.length - 1 && j < matrix[i].length - 1) {
+        if (matrix[i + 1][j + 1] === true) {
+          count++
+        }
+      }
+      matrixArr[i][j] = count
+      count = 0
+    }
+  }
+  return matrixArr
 }
 
 module.exports = {
-  minesweeper
-};
+  minesweeper,
+}
